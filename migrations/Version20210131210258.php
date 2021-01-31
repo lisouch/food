2960b06ba5892271ac8573bc153b7bedbb9f6b8d
lisouch <lisa.elsorogy@hotmail.fr>
2021-01-31 22:25:59 +0100

    fichiers cleanés / navbar connexion/deconnexion fonctionnelle

--- migrations/Version20210131210258.php
+++ migrations/Version20210131210258.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210131035425 extends AbstractMigration
+final class Version20210131210258 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20210131035425 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product ADD created_at DATETIME NOT NULL, ADD update_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD phone INT NOT NULL, ADD created_at DATETIME NOT NULL, ADD update_at DATETIME DEFAULT NULL, DROP roles');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product DROP created_at, DROP update_at');
+        $this->addSql('ALTER TABLE `user` ADD roles JSON NOT NULL, DROP phone, DROP created_at, DROP update_at');
     }
 }
--- public/js/lisa.js
+++ public/js/lisa.js
@@ -1,12 +1,13 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// // $('#navAnim').hide();
-// $('#logo').hide();
+$('#navAnim').hide();
+$('#logo').hide();
 
-// var $nav = $(".navbar-fixed-top");
-// var $navContainer = $(".navbar .container");
-// $(document).scroll(function () {
-//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-//     $navContainer.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-// });
-// });
+var $nav = $(".navbar-fixed-top");
+var $navContainer = $(".navbar .container");
+$(document).scroll(function () {
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $navContainer.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+});
+});
+
