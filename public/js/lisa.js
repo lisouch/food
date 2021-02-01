33da1ff794e36b0054b33ae2d1efb6a9263e6bcf
lisouch <lisa.elsorogy@hotmail.fr>
2021-02-01 09:38:27 +0100

    navbar accueil ok

--- public/js/lisa.js
+++ public/js/lisa.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
 
-$('#navAnim').hide();
-$('#logo').hide();
-
+// $('#navAnim').hide();
 var $nav = $(".navbar-fixed-top");
-var $navContainer = $(".navbar .container");
 $(document).scroll(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    $navContainer.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 });
 });
-
