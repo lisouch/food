eacc79e0581037e8e2de83e1bbf0844e4ca95240
lisouch <lisa.elsorogy@hotmail.fr>
2021-02-01 23:16:35 +0100

    navbar ok

--- public/js/lisa.js
+++ public/js/lisa.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 
 // $('#navAnim').hide();
 var $nav = $(".navbar-fixed-top");
+var $navLink = $(".nav-link");
 $(document).scroll(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $navLink.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 });
 });
