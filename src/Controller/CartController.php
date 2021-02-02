fca1de3f1776fe95521f197e9761fcfc85267e75
Charlotte Bardy <charlotte.bardy@gmail.com>
2021-02-02 12:00:22 +0100

    Gestion du panier / CartController/ CartService

---
+++ src/Controller/CartController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Service\Cart\CartService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/panier", name="cart_index")
+     */
+    public function index(CartService $cartService)
+    {
+        return $this->render('cart/cart.html.twig', [
+            'items' => $cartService->getFullCart(),
+            'total' => $cartService->getTotal()
+        ]);
+    }
+
+      /**
+     * @Route("/panier/add/{id}", name="cart_add")
+     */
+    public function add($id, CartService $cartService)
+    {
+        $cartService->add($id);
+
+        return $this->redirectToRoute('cart_index');
+    }
+
+     /**
+     * @Route("/panier/remove/{id}", name="cart_remove")
+     */
+    public function remove($id, CartService $cartService) 
+    {
+        $cartService->remove($id);
+       
+        return $this->redirectToRoute('cart_index');
+    }
+}
