13260f2313a7100c69b92fbfca7494b59ba3086e
lisouch <lisa.elsorogy@hotmail.fr>
2021-02-03 11:00:11 +0100

    tout est ok

--- public/js/lisa.js
+++ public/js/lisa.js
@@ -7,4 +7,28 @@ $(document).scroll(function () {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     $navLink.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 });
+    // Clique sur où le récupérer
+
+    $("#clickOnAdress").hide();
+
+    $("#where").click(function () {
+        $("#price").hide();
+        $("#quantity").hide();
+        $("#button").hide();
+        $("#clickOnAdress").toggle();
+    });
+
+    $("#returnButton").click(function () {
+        $("#price").toggle();
+        $("#quantity").toggle();
+        $("#button").toggle();
+        $("#clickOnAdress").hide();
+    });
+
+
+
+
+
+
+
 });
